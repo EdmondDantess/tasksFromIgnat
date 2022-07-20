68cfa0e46447372d9423936353624c55e24b755c
Maksim Laurouski <maximlavrovsky@mail.ru>
2022-07-20 16:26:59 +0300

    Updates

---
+++ precache-manifest.aef29f856344c0a381e38aa9ca085bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809d99168de49491e63efbc170c6294c",
+    "url": "/tasksFromIgnat/index.html"
+  },
+  {
+    "revision": "da490d5b0fd0ea10a0cb",
+    "url": "/tasksFromIgnat/static/css/main.e8dab02e.chunk.css"
+  },
+  {
+    "revision": "9446d5ffcd7e8fa30933",
+    "url": "/tasksFromIgnat/static/js/2.5fd48aaf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tasksFromIgnat/static/js/2.5fd48aaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da490d5b0fd0ea10a0cb",
+    "url": "/tasksFromIgnat/static/js/main.f066ea1d.chunk.js"
+  },
+  {
+    "revision": "efebbb8b9fd3651893c2",
+    "url": "/tasksFromIgnat/static/js/runtime-main.b6f33ee6.js"
+  }
+]);
